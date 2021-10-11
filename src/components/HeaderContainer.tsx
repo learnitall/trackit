d174e17ef96b75cfba780d16a50c78aeb3855362
Ryan Drew <ryan@thedrews.org>
2021-10-11 14:30:17 -0600

    Adjust header container and setup firebase

--- src/components/HeaderContainer.tsx
+++ src/components/HeaderContainer.tsx
@@ -1,11 +1,16 @@
-import { IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import { IonItem, IonThumbnail, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
 import './HeaderContainer.css';
 
 const HeaderContainer: React.FC = () => {
     return (
         <IonHeader>
             <IonToolbar>
-                <IonTitle>Track It</IonTitle>
+                <IonItem>
+                    <IonThumbnail>
+                        <img src={process.env.PUBLIC_URL + '/assets/icon/icon.png'} style={{ borderRadius: "5px"}}/>
+                     </IonThumbnail>
+                    <IonTitle>Track It</IonTitle>
+                </IonItem>
             </IonToolbar>
         </IonHeader>
     );
